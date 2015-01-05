ab77c2f1c278a0e5007570cd06eb74a85836d5e8
Al Lamarre <alamarre@gmail.com>
2015-01-05 01:57:05 -0500

    prevent double encoding of the query string

--- services/omny-potent/src/main/java/ca/omny/potent/RemoteUrlProvider.java
+++ services/omny-potent/src/main/java/ca/omny/potent/RemoteUrlProvider.java
@@ -7,9 +7,13 @@ import ca.omny.potent.models.OmnyEndpoint;
 import ca.omny.potent.models.OmnyRouteConfiguration;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
+import java.net.URLDecoder;
 import java.util.Collection;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,13 +30,19 @@ public class RemoteUrlProvider {
     OmnyRouteMapper routeMapper;
     
     public String getRemoteUrl(String route, HttpServletRequest req) {  
+        String decodedQueryString = req.getQueryString();
+        try {
+            decodedQueryString = URLDecoder.decode(decodedQueryString, "UTF-8");
+        } catch (UnsupportedEncodingException ex) {
+        }
         if(configurationReader.getSimpleConfigurationString("omny_route_dynamic") != null) {
             String key = db.getKey("omny_hosts","current");
             Type type = new TypeToken<List<RemoteHost>>(){}.getType();
             String value = db.get(key, String.class);
             List<RemoteHost> hosts = new Gson().fromJson(value, type);
             RemoteHost remoteHost = hosts.get((int)(Math.random()*hosts.size()));
-            return remoteHost.getProtocol()+"://"+remoteHost.getHostname()+":"+remoteHost.getPort()+route+ "?" + req.getQueryString();
+            
+            return remoteHost.getProtocol()+"://"+remoteHost.getHostname()+":"+remoteHost.getPort()+route+ "?" + decodedQueryString;
         }
         String host = "http://127.0.0.1:8077";
         if (configurationReader.getConfigurationString("proxyHost") != null) {
@@ -45,7 +55,7 @@ public class RemoteUrlProvider {
             return null;
         }
         
-        String remoteUrl = host + bestMatch.getContextRoot() + route + "?" + req.getQueryString();
+        String remoteUrl = host + bestMatch.getContextRoot() + route + "?" + decodedQueryString;
         
         return remoteUrl;
     }
