774b67efa1c642c497c69288f968e192a36d9a42
Al Lamarre <alamarre@gmail.com>
2015-03-27 01:15:40 -0400

    get docker properly

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo pip install awscli
 aws s3 cp output/omny-$1.zip s3://omny-releases/omny-$1.zip
-sudo apt-get install docker -y
+sudo wget -qO- https://get.docker.com/ | sh
 sudo docker build -t "alamarre/omny-cms:$1" .
 sudo docker login -e $DOCKER_EMAIL -p $DOCKER_PASSWORD
 sudo docker push alamarre/omny-cms:$1
