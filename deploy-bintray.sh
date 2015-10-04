e7ae8698d337b14fcc77ad63cb46a6a3898541ce
Al Lamarre <alamarre@gmail.com>
2015-10-04 17:12:29 -0400

    use S3 instead of bintray

--- deploy-bintray.sh
+++
@@ -1,6 +0,0 @@
-groupid=ca.omny
-package=$5
-version=$4
-group_subdir=`echo $groupid | tr . /`
-curl -T $1 -ualamarre:$3 https://api.bintray.com/maven/omny/omny/${package}/${group_subdir}/${package}/${version}/${package}-$version.jar
-curl -T $2 -ualamarre:$3 https://api.bintray.com/maven/omny/omny/${package}/${group_subdir}/${package}/${version}/${package}-${version}.pom
